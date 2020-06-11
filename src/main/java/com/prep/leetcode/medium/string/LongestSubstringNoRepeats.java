1d8f95f88019d08a845944415f0682e2d4796fcd
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-06-10 17:02:16 -0700

    testes pass

--- src/main/java/com/prep/leetcode/medium/string/LongestSubstringNoRepeats.java
+++ src/main/java/com/prep/leetcode/medium/string/LongestSubstringNoRepeats.java
@@ -1,4 +1,31 @@
 package com.prep.leetcode.medium.string;
 
 public class LongestSubstringNoRepeats {
+
+    public static int lengthOfLongestSubstring(String input) {
+        int maxLength = 0;
+        int currentLength = 0;
+        String substringWithoutRepeats = "";
+        String letter = "";
+        for (int i = 0; i < input.length(); i++) {
+
+            letter = input.substring(i, i + 1);
+
+            for (int j = i; j < input.length() ; j++) {
+                letter = input.substring(j, j + 1);
+                // break out of loop
+                if (substringWithoutRepeats.contains(letter)){
+                    currentLength = substringWithoutRepeats.length();
+                    break;
+                }else{
+                    substringWithoutRepeats = input.substring(i, j + 1);
+                }
+            }
+            // update max length
+            if (currentLength > maxLength){
+                maxLength = currentLength;
+            }
+        }
+        return maxLength;
+    }
 }
--- src/test/java/com/prep/leetcode/medium/string/LongestSubstringNoRepeatsTest.java
+++ src/test/java/com/prep/leetcode/medium/string/LongestSubstringNoRepeatsTest.java
@@ -1,7 +1,61 @@
 package com.prep.leetcode.medium.string;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LongestSubstringNoRepeatsTest {
 
+    //Alex Lundin
+    //OICE - An effective way to consume requirements
+    //Output
+    //  Make sure to write Output down
+    //Input
+    //  Make sure to write Input down
+    //Constraints
+    //  Are there any stated constraints to the problem?
+    //  Are there any indicators of time vs space trade-offs?
+    //Examples and Edge Cases
+    //  List examples from problem
+    //  Think of edge cases at boundary values, 0 and 1 are typical
+    String input;
+    int expected;
+    int actual;
+
+
+    @Test
+    public void lengthOfLongestSubstring_returnsThree() {
+        //Setup
+        input = "abcabcbb";
+        expected = 3;
+        //Exercise
+        actual = LongestSubstringNoRepeats.lengthOfLongestSubstring(input);
+        //Assert
+        assertEquals(expected, actual);
+        //Teardown
+    }
+
+    @Test
+    public void lengthOfLongestSubstring_returnsOne() {
+        //Setup
+        input = "bbbbb";
+        expected = 1;
+        //Exercise
+        actual = LongestSubstringNoRepeats.lengthOfLongestSubstring(input);
+        //Assert
+        assertEquals(expected, actual);
+        //Teardown
+    }
+
+    @Test
+    public void lengthOfLongestSubstring_pwwkew_returnsThree() {
+        //Setup
+        input = "pwwkew";
+        expected = 3;
+        //Exercise
+        actual = LongestSubstringNoRepeats.lengthOfLongestSubstring(input);
+        //Assert
+        assertEquals(expected, actual);
+        //Teardown
+    }
 }
