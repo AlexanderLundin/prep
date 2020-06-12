8f7122f461d1d20ae5f98658cd3c8a42cc90575e
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-06-12 16:12:36 -0700

    test complete

--- src/main/java/com/prep/interviews/strings/MettleTest2.java
+++ src/main/java/com/prep/interviews/strings/MettleTest2.java
@@ -1,4 +1,27 @@
 package com.prep.interviews.strings;
 
 public class MettleTest2 {
+    int[] leftRotate(int arr[], int d, int n)
+    {
+        for (int i = 0; i < d; i++)
+            arr = leftRotatebyOne(arr, n);
+        return arr;
+    }
+
+    int[] leftRotatebyOne(int arr[], int n)
+    {
+        int i, temp;
+        temp = arr[0];
+        for (i = 0; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        arr[i] = temp;
+        return arr;
+    }
+
+    void printArray(int arr[], int n)
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+
 }
--- src/test/java/com/prep/interviews/strings/MettleTest2Test.java
+++ src/test/java/com/prep/interviews/strings/MettleTest2Test.java
@@ -1,7 +1,36 @@
 package com.prep.interviews.strings;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MettleTest2Test {
+    //Alex Lundin
+    //OICE - An effective way to consume requirements
+    //Output
+    //  Make sure to write Output down
+    //Input
+    //  Make sure to write Input down
+    //Constraints
+    //  Are there any stated constraints to the problem?
+    //  Are there any indicators of time vs space trade-offs?
+    //Examples and Edge Cases
+    //  List examples from problem
+    //  Think of edge cases at boundary values, 0 and 1 are typical
+
+
+    @Test
+    public void leftRotateArray_withArray_returnsArray() {
+        //Setup
+        MettleTest2 rotate = new MettleTest2();
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        arr = rotate.leftRotate(arr, 2, 7);
+        rotate.printArray(arr, 7);
+        //Exercise
+
+        //Assert
+        //assertEquals(expected, actual);
+        //Teardown
+    }
 
 }
